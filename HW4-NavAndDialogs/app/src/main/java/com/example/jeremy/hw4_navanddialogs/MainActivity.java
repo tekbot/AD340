18d757c5b57c2d0dfe66127ac3d831b932b88197
Jeremy Duke <jeremy@tetrapod.io>
2017-05-04 21:48:14 -0700

    HW4 Functional toasts on Settings and About Us toolbar buttons

--- HW4-NavAndDialogs/app/src/main/java/com/example/jeremy/hw4_navanddialogs/MainActivity.java
+++ HW4-NavAndDialogs/app/src/main/java/com/example/jeremy/hw4_navanddialogs/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.jeremy.hw4_navanddialogs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater mMenuInflater = getMenuInflater();
+        mMenuInflater.inflate(R.menu.my_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_setting) {
+            Toast.makeText(MainActivity.this, "Settings Menu", Toast.LENGTH_SHORT).show();
+        }
+        if (item.getItemId() == R.id.action_about) {
+            Toast.makeText(MainActivity.this, "About Us", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
