4e837ac40ffed1444d1c681dc600585420aaed64
Jeremy Duke <jeremy@tetrapod.io>
2017-05-05 01:03:05 -0700

    HW4 - Implement FAB and logging on user choice, fix Recycler layout and add in slightly more interesting items

--- HW4-NavAndDialogs/app/src/main/java/com/example/jeremy/hw4_navanddialogs/MainActivity.java
+++ HW4-NavAndDialogs/app/src/main/java/com/example/jeremy/hw4_navanddialogs/MainActivity.java
@@ -2,18 +2,20 @@ package com.example.jeremy.hw4_navanddialogs;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +25,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
 
-        Button mShowDialog = (Button) findViewById(R.id.btn_alert_dialog);
-        mShowDialog.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
@@ -35,22 +37,24 @@ public class MainActivity extends AppCompatActivity {
                 mBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        Log.d(TAG, "PositiveButtonClicked");
                         dialog.dismiss();
                     }
                 });
                 mBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        Log.d(TAG, "NegativeButtonClicked");
                         dialog.dismiss();
                     }
                 });
                 mBuilder.setNeutralButton("Neutral", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        Log.d(TAG, "NeutralButtonClicked");
                         Toast.makeText(MainActivity.this, "This makes me feel ambivalent", Toast.LENGTH_SHORT).show();
                     }
                 });
-
                 AlertDialog alertDialog = mBuilder.create();
                 alertDialog.show();
             }
--- HW4-NavAndDialogs/app/src/main/java/com/example/jeremy/hw4_navanddialogs/RecyclerActivity.java
+++ HW4-NavAndDialogs/app/src/main/java/com/example/jeremy/hw4_navanddialogs/RecyclerActivity.java
@@ -6,11 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class RecyclerActivity extends AppCompatActivity {
@@ -19,17 +16,33 @@ public class RecyclerActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     RecyclerView.Adapter recyclerViewAdapter;
     RecyclerView.LayoutManager recylerViewLayoutManager;
-    String[][] subjects = {
-            {"ANDROID","1"},
-            {"PHP","2"},
-            {"BLOGGER","3"},
-            {"WORDPRESS", "4"},
-            {"JOOMLA", "5"},
-            {"ASP.NET", "6"},
-            {"JAVA", "7"},
-            {"C++", "8"},
-            {"JAVASCRIPT", "9"},
-            {"NODEJS", "10"}
+    String[][] books = {
+            {"Doctorow, Cory","Overclocked"},
+            {"Doctorow, Cory","Down and Out in the Magic Kingdom"},
+            {"Stross, Charles","Saturn's Children"},
+            {"Stross, Charles", "The Atrocity Archives"},
+            {"Stross, Charles", "Accelerando"},
+            {"Stross, Charles", "Glasshouse"},
+            {"Stross, Charles", "Iton Sunrise"},
+            {"Herbert, Frank", "Dune"},
+            {"Gaiman, Neil", "American Gods"},
+            {"Gaiman, Neil", "Good Omens"},
+            {"Gaiman, Neil", "Fragile Things"},
+            {"Repino, Robert", "MORT(E)"},
+            {"Weir, Andy", "The Martian"},
+            {"Cline, Ernest", "Ready Player One"},
+            {"Anderson, Poul", "Tau Zero"},
+            {"Asimov, Isaac", "Foundation Series"},
+            {"Gibson, William", "Neuromancer"},
+            {"Gibson, William", "The Difference Engine"},
+            {"Stephenson, Neal", "Anathem"},
+            {"Stephenson, Neal", "In the Beginning Was the Command Line"},
+            {"Stephenson, Neal", "Cobweb"},
+            {"Stephenson, Neal", "Snow Crash"},
+            {"Stephenson, Neal", "Quicksilver"},
+            {"Stephenson, Neal", "The Confusion"},
+            {"Stephenson, Neal", "The System of the World"},
+            {"Stephenson, Neal", "Seveneves"}
     };
 
     @Override
@@ -55,29 +68,25 @@ public class RecyclerActivity extends AppCompatActivity {
     }
 
     public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder> {
-
         public class ViewHolder extends RecyclerView.ViewHolder {
             // each data item is just a string in this case
-            public TextView mTitle;
-            public TextView mDetail;
+            TextView mAuthor;
+            TextView mTitle;
             public ViewHolder(View v) {
                 super(v);
-                mTitle = (TextView) v.findViewById(R.id.subject_1);
-                mDetail = (TextView) v.findViewById(R.id.subject_2);
+                mAuthor = (TextView) v.findViewById(R.id.subject_1);
+                mTitle = (TextView) v.findViewById(R.id.subject_2);
             }
         }
 
         @Override
-        public CustomAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
-
+        public CustomAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             // Inflate the view for this view holder
-            View item = getLayoutInflater().inflate(R.layout.list_item2, parent,
-                    false);
+            View item = getLayoutInflater().inflate(R.layout.list_item2, parent, false);
 
             // Call the view holder's constructor, and pass the view to it;
             // return that new view holder
-            ViewHolder vh = new ViewHolder(item);
-            return vh;
+            return new ViewHolder(item);
         }
 
         // Replace the contents of a view (invoked by the layout manager)
@@ -85,14 +94,17 @@ public class RecyclerActivity extends AppCompatActivity {
         public void onBindViewHolder(ViewHolder holder, int position) {
             // - get element from your dataset at this position
             // - replace the contents of the view with that element
-            holder.mTitle.setText(subjects[position][0]);
-            holder.mDetail.setText(subjects[position][1]);
+            String separator = "    -   ";
+            String author = books[position][0] + separator;
+            String title = books[position][1];
+            holder.mAuthor.setText(author);
+            holder.mTitle.setText(title);
         }
 
         // Return the size of your dataset (invoked by the layout manager)
         @Override
         public int getItemCount() {
-            return subjects.length;
+            return books.length;
         }
     }
 }
