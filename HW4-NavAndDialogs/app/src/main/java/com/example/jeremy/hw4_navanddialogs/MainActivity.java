a4ac7a21de7c8a871edec185834a57532b8026db
Jeremy Duke <jeremy@tetrapod.io>
2017-05-04 22:17:59 -0700

    Add in functional alert dialog from tutorials

--- HW4-NavAndDialogs/app/src/main/java/com/example/jeremy/hw4_navanddialogs/MainActivity.java
+++ HW4-NavAndDialogs/app/src/main/java/com/example/jeremy/hw4_navanddialogs/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.jeremy.hw4_navanddialogs;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +21,39 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
+
+        Button mShowDialog = (Button) findViewById(R.id.btn_alert_dialog);
+        mShowDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+                mBuilder.setIcon(android.R.drawable.sym_def_app_icon);
+                mBuilder.setTitle(R.string.dialog_title);
+                mBuilder.setMessage(R.string.dialog_message);
+                mBuilder.setCancelable(false);
+                mBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                mBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                mBuilder.setNeutralButton("Neutral", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "This makes me feel ambivalent", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                AlertDialog alertDialog = mBuilder.create();
+                alertDialog.show();
+            }
+        });
     }
 
     @Override
