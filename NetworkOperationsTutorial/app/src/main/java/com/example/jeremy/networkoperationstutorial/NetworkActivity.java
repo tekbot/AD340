2a687854d48cf8bc8eef7674a38749df81d9ae68
Jeremy Duke <jeremy@tetrapod.io>
2017-05-08 22:04:33 -0700

    Finish up Network Ops tutorial (consuming xml) and fix all the red (of which there was a ton)

--- NetworkOperationsTutorial/app/src/main/java/com/example/jeremy/networkoperationstutorial/NetworkActivity.java
+++ NetworkOperationsTutorial/app/src/main/java/com/example/jeremy/networkoperationstutorial/NetworkActivity.java
@@ -6,12 +6,21 @@ import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.webkit.WebView;
 
-/**
- * Created by jeremy on 5/7/17.
- */
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
 
 public class NetworkActivity extends Activity {
     public static final String WIFI = "Wi-Fi";
@@ -92,11 +101,94 @@ public class NetworkActivity extends Activity {
         if (((sPref.equals(ANY)) && (wifiConnected || mobileConnected))
                 || ((sPref.equals(WIFI)) && (wifiConnected))) {
             // AsyncTask subclass
-//            new DownloadXmlTask().execute(URL);
+            new DownloadXmlTask().execute(URL);
         } else {
 //            showErrorPage();
         }
     }
 
+    private class DownloadXmlTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return loadXmlFromNetwork(urls[0]);
+            } catch (IOException e) {
+                return getResources().getString(R.string.connection_error);
+            } catch (XmlPullParserException e) {
+                return getResources().getString(R.string.xml_error);
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            setContentView(R.layout.activity_main);
+            // Displays the HTML string in the UI via a WebView
+            WebView myWebView = (WebView) findViewById(R.id.webview);
+            myWebView.loadData(result, "text/html", null);
+        }
+
+        private String loadXmlFromNetwork(String urlString) throws XmlPullParserException, IOException {
+            InputStream stream = null;
+            // Instantiate the parser
+            StackOverflowXmlParser stackOverflowXmlParser = new StackOverflowXmlParser();
+            List<StackOverflowXmlParser.Entry> entries = null;
+            String title = null;
+            String url = null;
+            String summary = null;
+            Calendar rightNow = Calendar.getInstance();
+            DateFormat formatter = new SimpleDateFormat("MMM dd h:mmaa");
+
+            // Checks whether the user set the preference to include summary text
+            SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(NetworkActivity.this);
+            boolean pref = sharedPrefs.getBoolean("summaryPref", false);
+
+            StringBuilder htmlString = new StringBuilder();
+            htmlString.append("<h3>" + getResources().getString(R.string.page_title) + "</h3>");
+            htmlString.append("<em>" + getResources().getString(R.string.updated) + " " +
+                    formatter.format(rightNow.getTime()) + "</em>");
+
+            try {
+                stream = downloadUrl(urlString);
+                entries = stackOverflowXmlParser.parse(stream);
+                // Makes sure that the InputStream is closed after the app is
+                // finished using it.
+            } finally {
+                if (stream != null) {
+                    stream.close();
+                }
+            }
+
+            // StackOverflowXmlParser returns a List (called "entries") of Entry objects.
+            // Each Entry object represents a single post in the XML feed.
+            // This section processes the entries list to combine each entry with HTML markup.
+            // Each entry is displayed in the UI as a link that optionally includes
+            // a text summary.
+            for (StackOverflowXmlParser.Entry entry : entries) {
+                htmlString.append("<p><a href='");
+                htmlString.append(entry.link);
+                htmlString.append("'>" + entry.title + "</a></p>");
+                // If the user set the preference to include summary text,
+                // adds it to the display.
+                if (pref) {
+                    htmlString.append(entry.summary);
+                }
+            }
+            return htmlString.toString();
+        }
+
+        // Given a string representation of a URL, sets up a connection and gets
+        // an input stream.
+        private InputStream downloadUrl(String urlString) throws IOException {
+            java.net.URL url = new URL(urlString);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            return conn.getInputStream();
+        }
+    }
 }
 
